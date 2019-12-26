93b04567088277d5bba59cfd572606914227567f
LD <991988207@qq.com>
2019-12-26 17:00:45 +0800

    update

--- admin/src/views/bottomUser/owner/modules/uploadImg.vue
+++ admin/src/views/bottomUser/owner/modules/uploadImg.vue
@@ -24,7 +24,6 @@
           <input type="button" class="oper" style="height:32px;width:35px;font-size:18px;margin:3px 5px;background-color: #1890ff;color: #fff;border-radius: 4px;line-height: 26px;cursor: pointer;outline-offset: -4px;" value="↺" title="左旋转" @click="rotateLeft"/>
           <input type="button" class="oper" style="height:32px;width:35px;font-size:18px;margin:3px 5px;background-color: #1890ff;color: #fff;border-radius: 4px;line-height: 26px;cursor: pointer;outline-offset: -4px;" value="↻" title="右旋转" @click="rotateRight"/>
           <input type="button" class="oper" style="height:32px;width:35px;font-size:18px;margin:3px 5px;background-color: #1890ff;color: #fff;border-radius: 4px;line-height: 26px;cursor: pointer;outline-offset: -4px;" value="↓" title="下载" @click="down('blob')"/>
-          <!--<input type="button" class="btn btn-blue" value="上传头像" @click="finish('blob')">-->
           <div class="line" style="margin-top: 85px;text-align: left">
             <div class="cropper-content" style="margin-top:-60px;">
               <div class="cropper">
@@ -48,7 +47,8 @@
                 ></vueCropper>
               </div>
               <div style="margin-left:20px;">
-                <div class="show-preview" :style="{'width': '150px', 'height':'155px',  'overflow': 'hidden', 'margin': '5px'}">
+                <div class="show-preview"
+                     :style="{'width': '150px', 'height':'155px',  'overflow': 'hidden', 'margin': '5px'}">
                   <div :style="previews.div" class="preview">
                     <img :src="previews.url" :style="previews.img">
                   </div>
@@ -63,23 +63,23 @@
 </template>
 
 <script>
-  import {mixin} from '@/mixins/mixin'
-  import {mapState} from 'vuex';
-  import {ImageUpload} from '@/components'
+  import { mixin } from '@/mixins/mixin'
+  import { mapState } from 'vuex'
+  import { ImageUpload } from '@/components'
   import { VueCropper } from 'vue-cropper'
+
   export default {
-    mixins:[mixin],
+    mixins: [mixin],
     data () {
       return {
-        confirmLoading:false,
+        confirmLoading: false,
         visible: false,
-        headImg:'',
-        //剪切图片上传
+        headImg: '',
         crap: false,
         previews: {},
         option: {
           img: '',
-          outputSize:1, //剪切后的图片质量（0.1-1）
+          outputSize: 1, //剪切后的图片质量（0.1-1）
           full: false,//输出原图比例截图 props名full
           outputType: 'png',
           canMove: true,
@@ -90,11 +90,11 @@
           autoCropHeight: 200,
           fixedBox: true
         },
-        fileName:'',  //本机文件地址
+        fileName: '',  //本机文件地址
         downImg: '#',
-        imgFile:'',
-        uploadImgRelaPath:'', //上传后的图片的地址（不带服务器域名）
-        isCropper:false
+        imgFile: '',
+        uploadImgRelaPath: '', //上传后的图片的地址（不带服务器域名）
+        isCropper: false
       }
     },
     components: {
@@ -104,84 +104,82 @@
       ...mapState(['constants', 'system'])
     },
     methods: {
-
       add () {
         this.visible = true
         this.isCropper = true
         this.init()
       },
-      init(){
-        this.headImg =''
+      init () {
+        this.headImg = ''
         this.crap = false
         this.previews = {}
         this.option = {
-            img: '',
-            outputSize:1,
-            full: false,
-            outputType: 'png',
-            canMove: false,
-            original: false,
-            canMoveBox: true,
-            autoCrop: true,
-            autoCropWidth: 150,
-            autoCropHeight: 150,
-            fixedBox: true
+          img: '',
+          outputSize: 1,
+          full: false,
+          outputType: 'png',
+          canMove: false,
+          original: false,
+          canMoveBox: true,
+          autoCrop: true,
+          autoCropWidth: 150,
+          autoCropHeight: 150,
+          fixedBox: true
         }
-        this.uploadImgRelaPath =''
-        this.imgFile =''
+        this.uploadImgRelaPath = ''
+        this.imgFile = ''
         this.fileName = ''
       },
 
       //放大/缩小
-      changeScale(num) {
-        console.log('num',num)
-        num = num || 1;
-        this.$refs.cropper.changeScale(num);
+      changeScale (num) {
+        console.log('num', num)
+        num = num || 1
+        this.$refs.cropper.changeScale(num)
       },
 
       //坐旋转
-      rotateLeft() {
-        this.$refs.cropper.rotateLeft();
+      rotateLeft () {
+        this.$refs.cropper.rotateLeft()
       },
 
       //右旋转
-      rotateRight() {
-        this.$refs.cropper.rotateRight();
+      rotateRight () {
+        this.$refs.cropper.rotateRight()
       },
 
       //上传图片（点击上传按钮）
-      finish(type) {
-        let that = this;
+      finish (type) {
+        let that = this
         let base64 = ''
-        let formData = new FormData();
+        let formData = new FormData()
         // 输出
         if (type === 'blob') {
-
           this.$refs.cropper.getCropBlob((data) => {
             let img = window.URL.createObjectURL(data)
-            this.model = true;
-            this.headImg = img;
+            this.model = true
+            this.headImg = img
             var reader = new FileReader()
-            reader.readAsDataURL(data);
+            reader.readAsDataURL(data)
             that.confirmLoading = true
             reader.onload = function (e) {
               base64 = e.target.result
               console.log(base64)
               that.$api.face.canRegister({
-                imageBase64:base64
+                imageBase64: base64
               })
                 .then(resRes => {
-                  formData.append("file", data, that.fileName);
-                  formData.append("attOrigin", 'ADMIN');
-                  formData.append("attType", 'FACE');
-                  that.$api.face.upload(that.system.uploadMainUrl,formData)
+                  formData.append('file', data, that.fileName)
+                  formData.append('attOrigin', 'ADMIN')
+                  formData.append('attType', 'FACE')
+                  that.$api.face.upload(that.system.uploadMainUrl, formData)
                     .then(resUpl => {
                       that.$notification.success({
                         message: '成功',
                         description: `上传成功`
                       })
                       that.visible = false
-                      that.$emit('uploadFace',resUpl)
+                      that.$emit('uploadFace', resUpl)
                       that.isCropper = false
                       that.confirmLoading = false
                     })
@@ -192,19 +190,19 @@
           })
         } else {
           this.$refs.cropper.getCropData((data) => {
-            this.model = true;
-            this.modelSrc = data;
+            this.model = true
+            this.modelSrc = data
           })
         }
       },
 
       // 实时预览函数
-      realTime(data) {
+      realTime (data) {
         this.previews = data
       },
 
       //下载图片
-      down(type) {
+      down (type) {
         var aLink = document.createElement('a')
         aLink.download = 'author-img'
         if (type === 'blob') {
@@ -215,32 +213,32 @@
           })
         } else {
           this.$refs.cropper.getCropData((data) => {
-            this.downImg = data;
-            aLink.href = data;
+            this.downImg = data
+            aLink.href = data
             aLink.click()
           })
         }
       },
 
       //选择本地图片
-      uploadImg(e, num) {
-        var _this = this;
+      uploadImg (e, num) {
+        var _this = this
         //上传图片
         var file = e.target.files[0]
-        _this.fileName = file.name.split('.')[0]+'.png';
-        const isLt1M = file.size / 1024 / 1024 < 1;
+        _this.fileName = file.name.split('.')[0] + '.png'
+        const isLt1M = file.size / 1024 / 1024 < 1
         if (!isLt1M) {
-          this.$message.error('图片最大为1MB!');
+          this.$message.error('图片最大为1MB!')
           return false
         }
 
-        if (!/\.(gif|jpg|jpeg|png|GIF|JPG|PNG)$/.test(e.target.value)) {
-          alert('图片类型必须是.gif,jpeg,jpg,png中的一种')
+        if (!/\.(jpg|jpeg|png|JPEG|JPG|PNG)$/.test(e.target.value)) {
+          this.$message.error('图片类型必须是.jpg,jpeg,png中的一种')
           return false
         }
-        var reader = new FileReader();
-        reader.onload =(e) => {
-          let data;
+        var reader = new FileReader()
+        reader.onload = (e) => {
+          let data
           if (typeof e.target.result === 'object') {
             // 把Array Buffer转化为blob 如果是base64不需要
             data = window.URL.createObjectURL(new Blob([e.target.result]))
@@ -257,61 +255,58 @@
         // 转化为base64
         // reader.readAsDataURL(file)
         // 转化为blob
-        reader.readAsArrayBuffer(file);
-
+        reader.readAsArrayBuffer(file)
       },
 
       imgLoad (msg) {
         console.log(msg)
       },
 
-      handleCancelImg(){
+      handleCancelImg () {
         this.visible = false
         this.isCropper = false
       },
 
       handleSubmit () {
         const { form: { validateFields } } = this
-        // this.confirmLoading = true
         validateFields((errors, values) => {
           if (!errors) {
             this.visible = false
-            // this.confirmLoading = false
-            // this.form.resetFields()
-            // this.$emit('ok', values)
           } else {
             this.confirmLoading = false
           }
         })
       },
-
     }
   }
 </script>
 <style lang="less">
-  .cropper-content{
+  .cropper-content {
     display: flex;
     margin-left: 20px;
     display: -webkit-flex;
-    .cropper{
+    .cropper {
       width: 250px;
       height: 250px;
     }
-    .show-preview{
+    .show-preview {
       flex: 1;
       -webkit-flex: 1;
       display: flex;
       display: -webkit-flex;
       justify-content: center;
       -webkit-justify-content: center;
-      .preview{
+      .preview {
         overflow: hidden;
-        border:1px solid #cccccc;
+        border: 1px solid #cccccc;
         background: #cccccc;
         margin-left: 40px;
       }
     }
   }
-  .cropper-content .show-preview .preview {margin-left: 0;}
+
+  .cropper-content .show-preview .preview {
+    margin-left: 0;
+  }
 
 </style>
