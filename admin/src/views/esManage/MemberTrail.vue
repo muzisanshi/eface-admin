7931f278df73e1a30f49d3b488cf36c965e072db
muzisanshi <1052048489@qq.com>
2020-02-20 18:47:00 +0800

    去除百度定位图标和百度logo

--- admin/src/views/esManage/MemberTrail.vue
+++ admin/src/views/esManage/MemberTrail.vue
@@ -480,7 +480,7 @@
         geolocation.getCurrentPosition(function (r) {
           if (this.getStatus() == BMAP_STATUS_SUCCESS) {
             var mk = new BMap.Marker(r.point);
-            map.addOverlay(mk);
+            // map.addOverlay(mk);
             map.panTo(r.point);
             // alert('您的位置：' + r.point.lng + ',' + r.point.lat);
           } else {
